cf1cc13091d9ef12b4b785c1a7483758e12f1117
CollinHeist <collinheist@gmail.com>
2025-05-13 14:10:24 -0600

    Revise test database seeding
    Add more complicated models for improved testing

--- backend/app/db/test.py
+++ backend/app/db/test.py
@@ -4,14 +4,57 @@ from pathlib import Path
 from sqlalchemy.orm import Session
 
 from app.core.upload import add_transactions_to_database, parse_iccu_upload
-from app.models import Account, Expense, Income, Upload
+from app.models import Account, Expense, Income, Transaction, Upload
+
+
+TEST_TRANSACTIONS = [
+    # Groceries from checking
+    {
+        'date': date(2025, 4, 20),
+        'description': 'Groceries',
+        'note': '',
+        'amount': -85.55,
+        'account_id': 1,
+        'expense_id': None,
+        'income_id': None,
+    },
+    # Savings transfer from checking to savings
+    {
+        'date': date(2025, 5, 1),
+        'description': 'Savings Transfer',
+        'note': '',
+        'amount': -250,
+        'account_id': 1,
+        'expense_id': 3,
+        'income_id': None,
+    },
+    {
+        'date': date(2025, 5, 1),
+        'description': 'Savings Transfer',
+        'note': '',
+        'amount': 250,
+        'account_id': 2,
+        'expense_id': None,
+        'income_id': None,
+    },
+    # Paycheck into checking
+    {
+        'date': date(2025, 5, 14),
+        'description': 'Paycheck',
+        'note': '',
+        'amount': 2500,
+        'account_id': 1,
+        'expense_id': 1,
+        'income_id': None,
+    }
+]
 
 
 def initialize_test_data(db: Session) -> None:
 
     # Add test accounts
     account = Account(
-        name='ICCU Checking',
+        name='Checking',
         type='checking',
         account_number=1234567890,
         routing_number=9876543210,
@@ -26,12 +69,20 @@ def initialize_test_data(db: Session) -> None:
         interest=0.03,
     )
     db.add(savings_account)
+    credit_card = Account(
+        name='Credit Card',
+        type='credit',
+        account_number=1234567890,
+        routing_number=9876543210,
+        interest=0.249,
+    )
+    db.add(credit_card)
     db.commit()
 
     # Add test expenses
     car_loan = Expense(
         name='Car Loan',
-        description='AdelFi Tesla Loan',
+        description='Car Loan',
         amount=-950,
         type='monthly',
         start_date=date(2022, 1, 10),
@@ -42,21 +93,16 @@ def initialize_test_data(db: Session) -> None:
         transaction_filters=[],
     )
     db.add(car_loan)
-
     student_loan = Expense(
         name='Student Loan',
-        description='ICCU Student Loan',
+        description='Student Loan',
         amount=-350,
         type='monthly',
         start_date=date(2024, 6, 1),
         end_date=date(2028, 6, 1),
         from_account_id=account.id,
-        to_account_id=None,
-        change_schedule=[],
-        transaction_filters=[],
     )
     db.add(student_loan)
-
     savings = Expense(
         name='Savings',
         description='Savings Transfer',
@@ -66,18 +112,63 @@ def initialize_test_data(db: Session) -> None:
         end_date=date(2028, 6, 1),
         from_account_id=account.id,
         to_account_id=savings_account.id,
+    )
+    db.add(savings)
+    downpayment = Expense(
+        name='Downpayment',
+        description='Downpayment on new house',
+        amount=-25000,
+        type='one_time',
+        start_date=date(2025, 8, 1),
+        from_account_id=account.id,
+    )
+    db.add(downpayment)
+    car_insurance = Expense(
+        name='Car Insurance',
+        description='Car Insurance',
+        amount=-500,
+        type='recurring',
+        frequency={'value': 6, 'unit': 'months'},
+        start_date=date(2023, 1, 12),
+        from_account_id=credit_card.id,
+    )
+    db.add(car_insurance)
+
+    car_wash = Expense(
+        name='Car Wash',
+        description='Car Wash',
+        amount=-25,
+        type='monthly',
+        start_date=date(2024, 1, 10),
+        end_date=None,
+        from_account_id=credit_card.id,
+        to_account_id=None,
         change_schedule=[],
         transaction_filters=[],
     )
-    db.add(savings)
+    db.add(car_wash)
+    spotify = Expense(
+        name='Spotify',
+        description='Spotify',
+        amount=-14.99,
+        type='monthly',
+        start_date=date(2022, 1, 10),
+        end_date=None,
+        from_account_id=credit_card.id,
+        to_account_id=None,
+        change_schedule=[],
+        transaction_filters=[],
+    )
+    db.add(spotify)
+
     # Add test incomes
     ipco_income = Income(
-        name='Idaho Power Salary',
+        name='IPC Salary',
         amount=2950,
         type='recurring',
         frequency=14,
-        start_date=date(2021, 1, 10),
-        end_date=date(2045, 1, 10),
+        start_date=date(2021, 1, 14),
+        end_date=date(2045, 1, 14),
         account_id=account.id,
         change_schedule=[
             # Bonus
@@ -121,14 +212,19 @@ def initialize_test_data(db: Session) -> None:
 
     # Upload Transactions
     iccu_transactions = Path(__file__).parent.parent.parent.parent / 'iccu_2024_upload.csv'
-    upload = Upload(
-        filename=iccu_transactions.name,
-        data=iccu_transactions.read_bytes(),
-        upload_date=datetime.now(),
-        account_id=account.id,
-    )
-    db.add(upload)
-    transactions = parse_iccu_upload(upload)
-    add_transactions_to_database(transactions, upload.id, db)
+    if iccu_transactions.exists():
+        upload = Upload(
+            filename=iccu_transactions.name,
+            data=iccu_transactions.read_bytes(),
+            upload_date=datetime.now(),
+            account_id=account.id,
+        )
+        db.add(upload)
+        transactions = parse_iccu_upload(upload)
+        add_transactions_to_database(transactions, upload.id, db)
+    # Import hard-coded test transactions
+    else:
+        for transaction in TEST_TRANSACTIONS:
+            db.add(Transaction(**transaction))
 
     db.commit()
