69bfe15b96dafe32404224b8afd8d3c6727518bd
Collin Heist <collinheist@gmail.com>
2025-05-15 13:47:32 -0600

    Update layout.jsx

--- frontend/src/app/accounts/layout.jsx
+++ frontend/src/app/accounts/layout.jsx
@@ -1,7 +1,7 @@
 export default function AccountSummaryLayout({ children }) {
   return (
     <section className="flex flex-col gap-4 md:py-10">
-      <div className="inline-block max-w-lg">
+      <div className="inline-block">
         {children}
       </div>
     </section>
