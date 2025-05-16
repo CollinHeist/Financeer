f1b69eb475e78f6a9926ea41785fc59a7baf6a33
Collin Heist <collinheist@gmail.com>
2025-05-15 23:06:37 -0600

    Update transaction.py

--- backend/app/schemas/transaction.py
+++ backend/app/schemas/transaction.py
@@ -31,6 +31,7 @@ class ReturnRelatedTransactionSchema(BaseModel):
     id: int
     date: date_type
     description: str
+    note: str
     amount: float
 
 class ReturnTransactionSchemaNoAccount(BaseModel):
@@ -42,7 +43,7 @@ class ReturnTransactionSchemaNoAccount(BaseModel):
     account_id: int
     expense_id: int | None
     income_id: int | None
-    related_transactions: list[ReturnRelatedTransactionSchema]
+    related_transactions: list[ReturnRelatedTransactionSchema] = []
     related_to_transactions: list[ReturnRelatedTransactionSchema] = []
 
 class ReturnTransactionSchema(ReturnTransactionSchemaNoAccount):
@@ -53,6 +54,6 @@ class ReturnTransactionSchema(ReturnTransactionSchemaNoAccount):
 class ReturnUpcomingTransactionSchema(BaseModel):
     name: str
     amount: float
-    date: date
+    date: date_type
     expense_id: int | None = None
     income_id: int | None = None
