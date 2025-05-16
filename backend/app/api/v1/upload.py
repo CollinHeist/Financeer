4da0702ecba746bc486e0ac5f71dcf64773a1b76
Collin Heist <collinheist@gmail.com>
2025-05-15 23:04:43 -0600

    Create Citi transaction Upload API endpoint

--- backend/app/api/v1/upload.py
+++ backend/app/api/v1/upload.py
@@ -7,9 +7,10 @@ from app.core.upload import (
     add_transactions_to_database,
     create_upload,
     parse_generic_upload,
-    parse_iccu_upload,
 )
 from app.schemas.transaction import ReturnTransactionSchema
+from app.services.citi import parse_citi_upload
+from app.services.iccu import parse_iccu_upload
 
 
 upload_router = APIRouter(
@@ -55,5 +56,24 @@ def upload_iccu_transactions(
     upload = create_upload(file, account_id, db)
 
     transactions = parse_iccu_upload(upload)
+
+    return add_transactions_to_database(transactions, upload.id, db)
+
+
+@upload_router.post('/new/citi')
+def upload_citi_transactions(
+    file: UploadFile,
+    account_id: int = Query(...),
+    db: Session = Depends(get_database),
+) -> list[ReturnTransactionSchema]:
+    """
+    Upload an Citi Bank TransactionCSV file.
+
+    - account_id: The ID of the Account to upload the Transactions to.
+    """
+
+    upload = create_upload(file, account_id, db)
+
+    transactions = parse_citi_upload(upload)
 
     return add_transactions_to_database(transactions, upload.id, db)
---
+++ backend/app/services/citi.py
@@ -0,0 +1,43 @@
+from io import StringIO
+
+import pandas as pd
+
+from app.models.upload import Upload
+from app.schemas.transaction import NewTransactionSchema
+
+
+def parse_citi_upload(upload: Upload) -> list[NewTransactionSchema]:
+    """
+    Parse an Citi Bank Upload into a list of NewTransactionSchemas.
+
+    Args:
+        upload: The Upload to parse.
+
+    Returns:
+        A list of NewTransactionSchemas.
+    """
+
+    # Parse the raw Upload data into a CSV stream
+    file_stream = StringIO(upload.data.decode('utf-8'))
+    df = pd.read_csv(file_stream)
+
+    # Convert the posting date column to a datetime object
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+
+    # Convert the Debit and Credit columns to floats - convert NaN to 0
+    # remove rows where both are 0
+    df['Debit'] = df['Debit'].fillna(0).astype(float)
+    df['Credit'] = df['Credit'].fillna(0).astype(float)
+    df = df.loc[(df['Debit'] != 0) | (df['Credit'] != 0)]
+
+    return [
+        NewTransactionSchema(
+            date=row['Date'],
+            description=row['Description'],
+            # Citi Bank lists purchases as Debit (positive values), and
+            # payments as Credit (negative values).
+            amount=-(row['Debit'] or row['Credit']),
+            account_id=upload.account_id,
+        )
+        for _, row in df.iterrows()
+    ]
